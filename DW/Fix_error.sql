5d956f4fc5fcdb6f9aa14921ee74af05c2cda07d
Phakkhaphong Krittawat <phakkhaphong@msn.com>
2024-05-17 09:09:35 +0700

    Update Fix_error.sql

--- DW/Fix_error.sql
+++ DW/Fix_error.sql
@@ -19,4 +19,4 @@ select [DateKey],
     CONVERT(int,[MonthNumberOfYear]) as MonthNumberOfYear,
     'Y'+CONVERT(char(4),CalendarYear)+' '+'Q'+CONVERT(char(1),CalendarQuarter) as QuarterName,
     'Y'+CONVERT(char(4),CalendarYear) as YearName
-from duo.DimDate
+from dbo.DimDate
